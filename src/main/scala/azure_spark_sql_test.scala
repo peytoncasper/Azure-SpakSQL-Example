19ae6c823a1f37e78e076dffb4c5fe0b10ce15b0
Peyton Casper <peyton@PPC-Ubuntu-Desktop.attlocal.net>
2016-09-14 08:10:24 -0500

    Updates that add in additional hive interaction such ass creating, inserting, selecting, and reading hive tables

--- src/main/scala/azure_spark_sql_test.scala
+++ src/main/scala/azure_spark_sql_test.scala
@@ -6,6 +6,7 @@
 import com.datastax.spark.connector.SomeColumns
 import com.datastax.spark.connector._
 import com.datastax.spark.connector.cql.CassandraConnector
+import org.apache.spark.sql.SaveMode
 import org.apache.spark.sql.hive._
 import org.apache.spark.{SparkConf, SparkContext}
 
@@ -16,24 +17,37 @@ object azure_spark_sql_test {
       .setAppName("SparkSQLTest")
       .set("spark.cassandra.connection.host", "127.0.0.1")
       .setMaster("spark://127.0.0.1:7077")
-      .setMaster("local[1]")
-
-      .set("", "")
+//      .setMaster("local[1]")
     val sc = new SparkContext(sparkConf)
     val hiveContext = new HiveContext(sc)
     val hadoopConf=sc.hadoopConfiguration;
     hadoopConf.set("fs.azure.account.key.account_name.blob.core.windows.net", "key")
 
-    val data = sc.textFile("wasb://container@account_name.blob.core.windows.net/2016/08/29/00/1596422213_8d4bfbdeb0364be6913a80292a4ef606_1.csv")
+    val base_address = "wasb://container_name@account_name.blob.core.windows.net/"
+
+    val data = sc.textFile(base_address + "2016/08/29/00/1596422213_8d4bfbdeb0364be6913a80292a4ef606_1.csv")
     val seq_data = data.map(x => {
       val data = x.split(',')
       (data(0), data(1), data(2), data(2), data(3), data(4), data(5), data(6), data(7))
     })
 
     seq_data.foreach(string => println(string))
-    val peopleSchemaRDD =
 
-//
+    // Create an RDD From Raw Blob Storage data
+    val hiveRDD = hiveContext.createDataFrame(seq_data)
+
+    //Writing a new Spark SQL Created Hive Table
+    hiveRDD.write.orc(base_address + "test_hive_table")
+
+    // Reading from Spark SQL Created Hive Table
+    val createdTable = hiveContext.read.orc(base_address + "test_hive_table")
+
+    // Performing a SELECT statement on Hive Table
+    createdTable.select("_1")
+
+    // Inserting new data
+    hiveRDD.write.mode(SaveMode.Overwrite).orc(base_address + "test_hive_table")
+
 //    seq_data.saveToCassandra("danieltest", "events", SomeColumns("type", "user", "usertype", "success", "rk", "eventprocessedutctime", "partitionid", "eventenqueuedutctime"))
 
 
